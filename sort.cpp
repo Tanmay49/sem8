f360cc61d8afeb3a453a107e2739c2f7abde342b
Tanmay49 <tanmaykarmarkar49@gmail.com>
2024-04-26 16:47:01 +0530

    code fixes

--- sort.cpp
+++ sort.cpp
@@ -1,119 +1,154 @@
-#include<iostream>
-#include<vector>
-#include<omp.h>
+#include <iostream>
+#include <vector>
+#include <omp.h>
 
 using namespace std;
-void sbs(vector<int> arr){
-    int it=arr.size()-1;
-    for (int i=0;i<=it;i++){
-        for(int j=0;j<=it-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+void sbs(vector<int> arr)
+{
+    int it = arr.size() - 1;
+    for (int i = 0; i <= it; i++)
+    {
+        for (int j = 0; j < it - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
-    // for (int i=0;i<arr.size();i++){
-    //     cout<<arr[i]<<" ";
-    // }
-}
-void bs(vector<int> arr){
-    int it=arr.size()-1;
-    #pragma omp parallel
-    for (int i=0;i<=it;i++){
-        #pragma omp for
-        for(int j=0;j<=it-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
-            }
-        }
-    }
-
     // for (int i=0;i<arr.size();i++){
     //     cout<<arr[i]<<" ";
     // }
     // cout<<endl;
 }
+void bs(vector<int> arr)
+{
+    int it = arr.size() - 1;
+    // #pragma omp parallel
+    for (int i = 0; i <= it; i++)
+    {
+#pragma omp for
+        for (int j = 0; j < it - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+// #pragma omp critical
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
 
-void merge(vector<int> &arr,int start,int end,int mid){
+    // for (int i=0;i<arr.size();i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    cout << endl;
+}
+void merge(vector<int> &arr, int start, int end, int mid)
+{
     vector<int> L;
     vector<int> R;
 
-    for(int i=start;i<=mid;i++){
+    for (int i = start; i <= mid; i++)
+    {
         L.push_back(arr[i]);
-    }    
-    for(int i=mid+1;i<=end;i++){
+    }
+    for (int i = mid + 1; i <= end; i++)
+    {
         R.push_back(arr[i]);
-    }    
-    int i=0;
-    int j=0;
-    int k=start;
-    while(i<L.size()&& j< R.size()){
-        if(L[i]<=R[j]){
-            arr[k]=L[i];
-            i++;
-        }else{
-            arr[k]=R[j];
-            j++;
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = start;
+    while (i < L.size() && j < R.size())
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i++];
+        }
+        else
+        {
+            arr[k] = R[j++];
         }
         k++;
     }
-    while(i<L.size()){
-        arr[k]=L[i];
-        i++;
-        k++;
+    while (i < L.size())
+    {
+        arr[k++] = L[i++];
     }
-    while(j<R.size()){
-        arr[k]=R[j];
-        j++;
-        k++;
-    }
-    
-}
-void ms(vector<int> &arr,int start,int end){
-    if(start<end){
-        int mid=(start+end)/2;
-        ms(arr,start,mid);
-        ms(arr,mid+1,end);
-        merge(arr,start,end,mid);
+
+    while (j < R.size())
+    {
+        arr[k++] = R[j++];
     }
 }
-int main(){
-    int N=50000;
-    vector<int> arr;
-    vector<int> arr_cp;
-    for(int i=0;i<N;i++){
-        arr.push_back(rand()%N);
+void ms(vector<int> &arr, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = (start + end) / 2;
+        ms(arr, start, mid);
+        ms(arr, mid + 1, end);
+        merge(arr, start, end, mid);
     }
-    // arr_cp=arr;
-    // for(iter=arr.begin();iter!=arr.end();iter++){
-    //     cout<<*iter<<endl;
-    // }
-    // for (int i=0;i<arr.size();i++){
-    //     cout<<arr[i]<<" ";
-    // }
+}
 
-    cout<<endl;
-    // ms(arr,0,arr.size()-1);
-    // for (int i=0;i<arr.size();i++){
-    //     cout<<arr[i]<<" ";
-    // }
-    // vector<int>::iterator iter;
-    double start=omp_get_wtime();
-    sbs(arr);
-    double end=omp_get_wtime();
-    cout<<"Sequential bs"<<end-start<<endl;
-    
-    cout<<endl;
+void pms(vector<int> &arr, int start, int end)
+{
+    if (start < end)
+    {
+        int mid;
+        mid = (start + end) / 2;
+#pragma omp parallel sections
+{
+    #pragma omp section
+    {
+                pms(arr, start, mid);
+    }
+    #pragma omp section           
+{                pms(arr, mid + 1, end);
 
-    // for (int i=0;i<arr.size();i++){
+}
+
+}
+    merge(arr, start, end, mid);
+
+    }
+}
+int main()
+{
+
+    const int size = 5000000; // Size of the array
+    vector<int> arr(size), arr_copy(size);
+
+    // Initialize the array with random values
+    for (int i = 0; i < size; ++i)
+    {
+        arr[i] = rand() % 1000;
+        arr_copy[i] = arr[i];
+    }
+
+    double start = omp_get_wtime();
+    // sbs(arr);
+    double end = omp_get_wtime();
+    cout << "Sequential bs" << end - start << endl;
+
+    start = omp_get_wtime();
+    bs(arr_copy);
+    end = omp_get_wtime();
+    cout << "Parallel bs" << end - start << endl;
+
+    start = omp_get_wtime();
+    ms(arr, 0, arr.size() - 1);
+    end = omp_get_wtime();
+    std::cout << "Sequential" << end - start << endl;
+    // for(int i=0;i<arr.size();i++){
     //     cout<<arr[i]<<" ";
     // }
-    cout<<endl;
-    start=omp_get_wtime();
-    bs(arr);
-    end=omp_get_wtime();
-    cout<<"Parallel bs"<<end-start<<endl;
-        
+    start = omp_get_wtime();
+    pms(arr_copy, 0, arr_copy.size() - 1);
+    end = omp_get_wtime();
+    std::cout << "Parallel" << end - start << endl;
 
     return 0;
 }
